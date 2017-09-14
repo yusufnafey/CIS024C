769b9f3ff2a80f918885f6ba9dee5f1193c8c533
yusufnafey <yusufnafey@yahoo.com>
2017-09-13 23:58:40 -0700

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,93 @@
+
+# coding: utf-8
+
+# In[1]:
+
+a = 5
+b = 6
+print a,b
+
+
+# In[2]:
+
+greeting = "Hello"
+person = "Oscar"
+
+
+# In[14]:
+
+personalGreeting = greeting + " " + person
+print personalGreeting
+
+
+# In[18]:
+
+input("Please enter your age: ")
+
+
+# In[19]:
+
+age = int(input("Please enter your age: "))
+
+
+# In[21]:
+
+if age >= 35:
+    print ("The age is greater than 35")
+else:
+    print ("The age is not greater than 35")
+
+
+# In[22]:
+
+number = 10
+number/4
+
+
+# In[23]:
+
+number//4
+
+
+# In[24]:
+
+newNumber = number%4
+print newNumber
+
+
+# In[25]:
+
+number = 101.35
+number/4
+
+
+# In[26]:
+
+number//4
+
+
+# In[27]:
+
+newNumber = number%4
+print newNumber
+
+
+# In[28]:
+
+myAge = 21
+yourAge = 25
+result = myAge==yourAge
+print result
+
+
+# In[29]:
+
+hisAge = 25
+result = myAge==hisAge
+print result
+
+
+# In[ ]:
+
+
+
