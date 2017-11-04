406c8a75075db66e313c1cdab0972c96459bfa7b
yusufnafey <yusufnafey@yahoo.com>
2017-11-03 23:49:57 -0700

    Add files via upload

---
+++ helperfunctions.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+def add(n1,n2):
+    return n1 + n2
+
+def diff(n1,n2):
+    return n1 - n2
+
+def product(n1,n2):
+    return n1 * n2
+
+def greatest(n1,n2):
+    if n1 > n2:
+        highest = n1
+    else:
+        highest = n2
+    return highest
+
